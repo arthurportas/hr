83d732fab555fbc3d5f5e43eea13632abd50d42c
Arthur Portas <arthurportas@gmail.com>
2014-04-12 14:31:11 +0100

    Deploy

--- src/main/java/com/homerenting/mvc/HomeController.java
+++ src/main/java/com/homerenting/mvc/HomeController.java
@@ -1,5 +1,7 @@
 package com.homerenting.mvc;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
@@ -9,16 +11,18 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 public class HomeController {
 
+    private static final Logger slf4jLogger = LoggerFactory.getLogger(HomeController.class);
 
     @Autowired
     public HomeController() {
     }
 
-    @RequestMapping(value = "home", method = RequestMethod.GET)
+    @RequestMapping(value = "/home", method = RequestMethod.GET)
     @ResponseStatus(HttpStatus.OK)
     @ResponseBody
     public String displayHome(Model model) {
+        slf4jLogger.info("/home");
+        slf4jLogger.info("String displayHome(Model model) ");
         return "index";
     }
-
 }
